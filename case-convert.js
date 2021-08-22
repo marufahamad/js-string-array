2e927ec645c0e26132a3f371093872dbda37fb21
unknown <maruf.ahamad@gmail.com>
2021-08-22 11:46:02 +0600

    first commit

---
+++ case-convert.js
@@ -0,0 +1,8 @@
+const anthem = 'Amar Sonar Bangla Ami Tomai Valobashi';
+const search = 'valo';
+const userInput = 'blACkPinK'
+const savedUserName = 'blackPink'
+if (userInput.toLocaleLowerCase() == savedUserName.toLocaleLowerCase()) {
+    // console.log('user exists')
+} 
+
